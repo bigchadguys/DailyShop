1fbbedf1249da80f50207e28f836ae0e8f3590cc
KaptainWutax <36993134+KaptainWutax@users.noreply.github.com>
2024-05-22 00:30:34 -0400

    fixed sided issue

--- common/src/main/java/bigchadguys/dailyshop/net/ModPacket.java
+++ common/src/main/java/bigchadguys/dailyshop/net/ModPacket.java
@@ -3,6 +3,10 @@ package bigchadguys.dailyshop.net;
 import bigchadguys.dailyshop.DailyShopMod;
 import bigchadguys.dailyshop.data.bit.ArrayBitBuffer;
 import bigchadguys.dailyshop.data.serializable.IBitSerializable;
+import dev.architectury.platform.Platform;
+import dev.architectury.utils.Env;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.PacketByteBuf;
@@ -27,36 +31,55 @@ public abstract class ModPacket<T extends PacketListener> implements Packet<T>, 
         this.readBits(buffer);
     }
 
+    @Override
     public void apply(T listener) {
-        ThreadExecutor<?> engine = null;
-
-        if(listener instanceof ServerPlayNetworkHandler handler) {
-            engine = handler.player.getServer();
-        } else if(listener instanceof ClientPlayNetworkHandler) {
-            engine = MinecraftClient.getInstance();
-        } else {
-            DailyShopMod.LOGGER.error("Failed to handle packet {}, engine {} is unknown", this, listener);
-        }
+        ThreadExecutor<?> engine = switch(Platform.getEnvironment()) {
+            case CLIENT -> this.getClientEngine(listener);
+            case SERVER -> this.getServerEngine(listener);
+        };
 
         if(engine == null || engine.isOnThread()) {
             this.onReceive(listener);
-        } else {
-            engine.executeSync(() -> {
-                if(listener.isConnectionOpen()) {
-                    try {
-                        this.apply(listener);
-                    } catch(Exception exception) {
-                        if(listener.shouldCrashOnException()) {
-                            throw exception;
-                        }
-
-                        DailyShopMod.LOGGER.error("Failed to handle packet {}, suppressing error", this, exception);
-                    }
-                } else {
-                    DailyShopMod.LOGGER.debug("Ignoring packet due to disconnection: {}", this);
-                }
-            });
+            return;
         }
+
+        engine.executeSync(() -> {
+            if(listener.isConnectionOpen()) {
+                try {
+                    this.apply(listener);
+                } catch(Exception exception) {
+                    if(listener.shouldCrashOnException()) {
+                        throw exception;
+                    }
+
+                    DailyShopMod.LOGGER.error("Failed to handle packet {}, suppressing error", this, exception);
+                }
+            } else {
+                DailyShopMod.LOGGER.debug("Ignoring packet due to disconnection: {}", this);
+            }
+        });
+    }
+
+    @Environment(EnvType.SERVER)
+    public ThreadExecutor<?> getServerEngine(T listener) {
+        if(listener instanceof ServerPlayNetworkHandler handler) {
+            return handler.player.getServer();
+        }
+
+        DailyShopMod.LOGGER.error("Failed to handle packet {}, engine {} is unknown", this, listener);
+        return null;
+    }
+
+    @Environment(EnvType.CLIENT)
+    public ThreadExecutor<?> getClientEngine(T listener) {
+        if(listener instanceof ServerPlayNetworkHandler handler) {
+            return handler.player.getServer();
+        } else if(listener instanceof ClientPlayNetworkHandler) {
+            return MinecraftClient.getInstance();
+        }
+
+        DailyShopMod.LOGGER.error("Failed to handle packet {}, engine {} is unknown", this, listener);
+        return null;
     }
 
 }
